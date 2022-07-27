64257f07e186100d40de6fddaa979936307b5221
arjunthumbayil <arjunthumbayil@gmail.com>
2022-07-27 18:29:09 +0530

    correcting responsiveness

---
+++ pages/shipping.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const shipping = () => {
+  return (
+    <div className="flex justify-center items-center ">
+      To be developed in Version 2. Kindly traverse back. Thanks.
+    </div>
+  );
+};
+
+export default shipping;
